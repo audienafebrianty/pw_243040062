6e05cfa48997c8be94a1c946967bdebab67462db
audienafebrianty <febriantyaudiena@gmail.com>
2025-02-13 21:34:24 -0800

    baru

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Audiena Febrianty";
+    $email = "febriantyaudiena@gmail.com";
+    $pekerjaan = "mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, nama saya $nama, saya adalah $pekerjaan di $jurusan $universitas.</p>";
+?>
